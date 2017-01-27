c0b90af2a063a586e1ecb7b296092286d9b33994
NickNaso <nicoladelgobbo@gmail.com>
2017-01-27 04:28:09 +0100

    Added function library for stdio Functions to best redirect the stdio using Ghostscript command

---
+++ src/ghostscript.c
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Nicola Del Gobbo
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the license at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+ * IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+ * MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ *
+ * Contributors - initial API implementation:
+ * Nicola Del Gobbo <nicoladelgobbo@gmail.com>
+ ******************************************************************************/
+
+/* Functions library to best redirect the stdio using Ghostscript */
+
+static int GSDLLCALL
+gsdll_stdin(void *instance, char *buf, int len)
+{
+    int ch;
+    int count = 0;
+    while (count < len) {
+	ch = fgetc(stdin);
+	if (ch == EOF)
+	    return 0;
+	*buf++ = ch;
+	count++;
+	if (ch == '\n')
+	    break;
+    }
+    return count;
+}
+
+static int GSDLLCALL
+gsdll_stdout(void *instance, const char *str, int len)
+{
+    fwrite(str, 1, len, stdout);
+    fflush(stdout);
+    return len;
+}
+
+static int GSDLLCALL
+gsdll_stderr(void *instance, const char *str, int len)
+{
+    fwrite(str, 1, len, stderr);
+    fflush(stderr);
+    return len;
+}
