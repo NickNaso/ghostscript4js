3255a1c9efd9a7085eb304c66e42c2602110674e
NickNaso <nicoladelgobbo@gmail.com>
2017-02-02 01:24:36 +0100

    Update header comment

--- src/ghostscript.c
+++ src/ghostscript.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Nicola Del Gobbo
+ * Copyright (c) 2017 Nicola Del Gobbo
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the license at http://www.apache.org/licenses/LICENSE-2.0
